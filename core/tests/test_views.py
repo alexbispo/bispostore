25b2da8c1033ff5c63904a69c4de25450bb86a75
alexbispo <alexbispo.artes@gmail.com>
2017-09-07 02:39:45 -0300

    Add tests for core views.

---
+++ core/tests/test_views.py
@@ -0,0 +1,74 @@
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('index_core')
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'index.html')
+
+
+class ContactViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('contact_core')
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'contact.html')
+
+
+class ProductViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('product_core')
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'product.html')
+
+
+class ProductListViewTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('product_list_core')
+
+    def tearDown(self):
+        pass
+
+    def test_status_code(self):
+        response = self.client.get(self.url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+        self.assertTemplateUsed(response, 'product_list.html')
