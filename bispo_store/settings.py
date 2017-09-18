f3d34c88b5af2f9c9b15729d8c02116bfe63ebe6
alexbispo <alexbispo.artes@gmail.com>
2017-09-17 22:20:34 -0300

    Adjustments to deploy on heroku production with gunicorn

--- bispo_store/settings.py
+++ bispo_store/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'j065bidlf_)cue_6k34cz6$nx0g-lt8pvy&-2^t)5h#1yvlc49'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
